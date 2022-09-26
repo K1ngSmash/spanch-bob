cf288aaa4e6a9030bb408358e06930ad9a2201cd
Zakir <107127152+K1ngSmash@users.noreply.github.com>
2022-09-26 20:04:06 +0600

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,3 +1,120 @@
+"use strict";
+
+/*Анимации при скроле*/
+const animItems = document.querySelectorAll("._anim-items");
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll(params) {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        pageYOffset > animItemOffset - animItemPoint &&
+        pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("_active");
+      } else {
+        if (!animItem.classList.contains("_anim-no-hide")) {
+          animItem.classList.remove("_active");
+        }
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+  }
+  setTimeout(() => {
+    animOnScroll();
+  }, 300);
+}
+
+/**/
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  IOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.IOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
+
+if (isMobile.any()) {
+  document.body.classList.add("_touch");
+} else {
+  document.body.classList.add("_pc");
+}
+
+/*Не заработало...*/
+const menuLinks = document.querySelectorAll(".navbar__links [data-goto]");
+if (menuLinks.length > 0) {
+  menuLink.forEach((element) => {
+    menuLinks.addEventListener("click", onMenuLInkClick);
+  });
+
+  function onMenuLInkClick(e) {
+    const menuLink = e.target;
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        pageYOffset -
+        document.querySelector(".header").offsetHeight;
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth",
+      });
+      e.perventDefault();
+    }
+  }
+}
+/*Не заработало...*/
+
+/*Модальное окно*/
+const iconMenu = document.querySelector(".navbar__burger");
+if (iconMenu) {
+  const menuBody = document.querySelector(".navbar");
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+}
+
+/*Перекидывает вас наверх*/
 window.addEventListener("scroll", function () {
   document.getElementById("showScroll").innerHTML = pageYOffset + "px";
 });
